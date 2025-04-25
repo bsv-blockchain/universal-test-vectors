a0c6591839476a1940a5fbbe8fb1226dea8ad129
Jason Chavannes <jason.chavannes@gmail.com>
2025-04-25 09:52:44 -0700

    Update CreateSignature vector

--- brc100frames/create-signature.ts
+++ brc100frames/create-signature.ts
@@ -14,7 +14,7 @@ export const createSignature: Record<string, ReturnType<typeof generateWireFrame
         'createSignature',
         {
             // Byte array representation for wire encoding
-            signature: hexToBytes("3044022004000100020003000400050006000700080009000a000b000c000d000e000f00101112131415161718191a1b1c1d1e1f202122232425262728292a2b2c2d2e2f30310220ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"),
+            signature: hexToBytes("302502204e45e16932b8af514961a1d3a1a25fdf3f4f7732e9d624c6c61548ab5fb8cd41020100"),
         },
         {
             protocolID: [1 /* SecurityLevelEveryApp */, 'test-protocol'],
