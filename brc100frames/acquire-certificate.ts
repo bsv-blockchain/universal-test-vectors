fa3f5c226e5d90630fe5ba064633e6ebdbef3aa2
Jason Chavannes <jason.chavannes@gmail.com>
2025-07-02 12:00:31 -0700

    Split AcquireCertificate tests

--- brc100frames/acquire-certificate.ts
+++ brc100frames/acquire-certificate.ts
@@ -25,14 +25,36 @@ export const acquireCertificate: Record<string, ReturnType<typeof generateWireFr
             // Args object (WalletAcquireCertificateArgs) - JSON format
             type: TypeBase64,
             certifier: CertifierPubKeyHex,
-            acquisitionProtocol: "issuance",
+            acquisitionProtocol: "direct",
             fields: {email: "alice@example.com", name: "Alice"}, // Alphabetical
             serialNumber: SerialNumberBase64,
             revocationOutpoint: RevocationOutpoint,
             signature: "3045022100a6f09ee70382ab364f3f6b040aebb8fe7a51dbc3b4c99cfeb2f7756432162833022067349b91a6319345996faddf36d1b2f3a502e4ae002205f9d2db85474f9aed5a",
-            certifierUrl: "https://certifier.example.com",
             keyringRevealer: "025ad43a22ac38d0bc1f8bacaabb323b5d634703b7a774c4268f6a09e4ddf79097",
             keyringForSubject: {field1: "key1", field2: "key2"},
+            privileged: false,
+        } as AcquireCertificateArgs
+    ),
+    'acquireCertificate-issuance': generateWireFramesFor(
+        'acquireCertificate',
+        {
+            // Result object (WalletCertificate) - JSON format
+            type: TypeBase64,
+            serialNumber: SerialNumberBase64,
+            subject: SubjectPubKeyHex,
+            certifier: CertifierPubKeyHex,
+            revocationOutpoint: RevocationOutpoint,
+            fields: {email: "alice@example.com", name: "Alice"}, // Alphabetical
+            // signature pk = 95c5931552e547d72a292e9d6f59eef2b9f7e1576d8c7b49731b505117c0cdfa, msg = test message
+            signature: "3045022100a6f09ee70382ab364f3f6b040aebb8fe7a51dbc3b4c99cfeb2f7756432162833022067349b91a6319345996faddf36d1b2f3a502e4ae002205f9d2db85474f9aed5a",
+        } as AcquireCertificateResult,
+        {
+            // Args object (WalletAcquireCertificateArgs) - JSON format
+            type: TypeBase64,
+            certifier: CertifierPubKeyHex,
+            acquisitionProtocol: "issuance",
+            fields: {email: "alice@example.com", name: "Alice"}, // Alphabetical
+            certifierUrl: "https://certifier.example.com",
             privileged: false,
         } as AcquireCertificateArgs
     ),
